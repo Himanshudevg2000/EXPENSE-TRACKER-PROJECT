ded7024bffa75f980aa13419e3518a85d1363924
Himanshudevg2000 <himanshudevgade87@gmail.com>
2022-09-15 19:37:05 +0530

    login page handling cases added

--- controllers/logindetail.js
+++ controllers/logindetail.js
@@ -1,5 +1,14 @@
 const User = require('../models/user')
 
+function isstringinvalid(string){
+    if(string == undefined || string.length === 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
 exports.signup = (req,res) => {
     const name = req.body.name;
     const email = req.body.email;
@@ -23,15 +32,23 @@ exports.login = (req,res) => {
     const email = req.body.email;
     const password = req.body.password;
 
+    if(isstringinvalid(email) || isstringinvalid(password)){
+        return res.status(404).json({success: false, message:'emailid or password is missing'})
+    }
+
     User.findAll({where : {email,password}})
         .then(response => {
-            if(response) {
-            res.status(200).json({success:true, message:'login Successfully'})
+            if(response.length > 0){
+                if(response[0].password === password) {
+                    res.status(200).json({success:true, message:'login Successfully'})
+                }else{
+                    return res.status(400).json({success: false, message:'password is incorrect'})
+                }
             }else{
-                throw new Error('user doesnt exist')
+                return res.status(404).json({success: false, message: 'User does not exist'})
             }
         })
         .catch(err => {
-            res.status(500).json({success:false,message: 'Somthing is wrong'})
+            res.status(500).json({success:false,message: err})
         })
 }
